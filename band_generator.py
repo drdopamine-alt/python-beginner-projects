5d743511d795ea9c7ad90bc197dff3b5d032c3a7
drdopamine-alt <yadavmainakshi1706@gmail.com>
2026-04-17 11:31:16 +0530

    Added band generator

---
+++ band_generator.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print(f"Good to know , your name is:{name}")
+city = input("Enter your city name: ")
+print("Good to know you are from", city)
+pet_name = input("Enter your pet name: ")
+print("Ooh you have",pet_name)
+
+print("Your band name is:", city+pet_name)
