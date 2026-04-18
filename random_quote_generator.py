fc20daf6f162caebef60d67d6f031bed3ebbbb56
drdopamine-alt <yadavmainakshi1706@gmail.com>
2026-04-18 15:05:02 +0530

    Added quote generator using random library

---
+++ random_quote_generator.py
@@ -0,0 +1,3 @@
+import random
+quotes = ['Honesty is the best policy.','Education is the most powerful weapon to change the world.','Charity begins at home.','Mind your own business.','Never put your leg in other\'s business','It\'s funny how you\'re nice to my face, but a snake on my back.']
+print(f'Aaj ka gyan:{random.choice(quotes)}')
